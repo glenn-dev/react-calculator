3a24af1e20868e762f229abb5270e83a4aa38730
glenn-dev <glenn.marcano90@gmail.com>
2020-06-03 12:58:07 -0400

    calculator component: show clicked elements

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -3,6 +3,18 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 
 const Calculator = () => {
+  const inputElement = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
+  const resolveInput = () => {
+    console.log('resolving...')
+  }
+
+  const clearInput = () => {
+    console.log('clearing input...')
+  }
   return (
     <Container className="mt-5">
       <Row>
@@ -17,71 +29,71 @@ const Calculator = () => {
             <div className="col-8 p-0">
               <div className="row m-0 m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">+</button>
+                  <button className="button" value={'+'} onClick={inputElement}>+</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">-</button>
+                  <button className="button" value={'-'} onClick={inputElement}>-</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">x</button>
+                  <button className="button" value={'*'} onClick={inputElement}>x</button>
                 </div>
               </div>
 
               <div className="row m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">7</button>
+                  <button className="button" value={'7'} onClick={inputElement}>7</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">8</button>
+                  <button className="button" value={'8'} onClick={inputElement}>8</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">9</button>
+                  <button className="button" value={'9'} onClick={inputElement}>9</button>
                 </div>      
               </div>
 
               <div className="row m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">4</button>
+                  <button className="button" value={'4'} onClick={inputElement}>4</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">5</button>
+                  <button className="button" value={'5'} onClick={inputElement}>5</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">6</button>
-                </div>
-              </div>
-
-              <div className="row m-0">
-                <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">1</button>
-                </div>
-                <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">2</button>
-                </div>
-                <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">3</button>
+                  <button className="button" value={'6'} onClick={inputElement}>6</button>
                 </div>
               </div>
 
               <div className="row m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">0</button>
+                  <button className="button" value={'1'} onClick={inputElement}>1</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button">.</button>
+                  <button className="button" value={'2'} onClick={inputElement}>2</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button ac-button">AC</button>
+                  <button className="button" value={'3'} onClick={inputElement}>3</button>
+                </div>
+              </div>
+
+              <div className="row m-0">
+                <div className="col-2 my-2 mx-3 p-0">
+                  <button className="button" value={'0'} onClick={inputElement}>0</button>
+                </div>
+                <div className="col-2 my-2 mx-3 p-0">
+                  <button className="button" value={'.'} onClick={inputElement}>.</button>
+                </div>
+                <div className="col-2 my-2 mx-3 p-0">
+                  <button className="button ac-button" value={'ac'} onClick={clearInput}>AC</button>
                 </div>
               </div>
             </div>
 
             <div className="col-2 p-0">
               <div className="my-2 mx-3 p-0">
-                  <button className="button">/</button>
+                  <button className="button" value={'/'} onClick={inputElement}>/</button>
               </div>
               <div className="my-3 my-2 mx-3 p-0">
-                <button className="button equal-button">=</button>
+                <button className="button equal-button" onClick={resolveInput}>=</button>
               </div>
             </div>
 
