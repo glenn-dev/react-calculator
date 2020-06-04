c8aa10bc295e4654d6d01c27a8820e0c0efe0613
glenn-dev <glenn.marcano90@gmail.com>
2020-06-03 22:18:29 -0400

    calculator component: all functionalities working

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 
 const Calculator = () => {
-  const inputElement = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
-  }
+  
+  const [inputValue, setInputValue] = useState('');
+  const [operator, setOperatorValue] = useState(null);
+  
+  const inputElement = (elem) => {
+    let newValue = inputValue + elem;
+    setInputValue(newValue);
+    return inputValue;
+  };
 
-  const resolveInput = () => {
-    console.log('resolving...')
-  }
+  const resolveInput = (inputValue, operator) => {
+    console.log(`resolving: ${inputValue}`);
+    operator != undefined ? setInputValue(`${eval(inputValue)} ${operator}`) : setInputValue(eval(inputValue));
+    setOperatorValue(null);
+  };
 
   const clearInput = () => {
-    console.log('clearing input...')
-  }
+    setInputValue('');
+    setOperatorValue(null);
+  };
+
+  const operatorElement = (elem) => {
+    inputElement(elem);
+    operator != null ? resolveInput(inputValue, elem) : setOperatorValue(elem);
+  };
+
   return (
     <Container className="mt-5">
       <Row>
         <div className="calculator text-center">
 
           <div className=" row m-0 w-100">
-            <input className="mb-2 w-100 d-block bg-dark text-white input" type="text"></input>
+            <input 
+              className="mb-2 w-100 d-block bg-dark text-white input" 
+              type="text" 
+              placeholder={'0'}
+              value={inputValue}>
+            </input>
           </div>
 
           <div className="row m-0">
@@ -29,71 +48,71 @@ const Calculator = () => {
             <div className="col-8 p-0">
               <div className="row m-0 m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'+'} onClick={inputElement}>+</button>
+                  <button className="button" onClick={() => operatorElement('+')}>+</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'-'} onClick={inputElement}>-</button>
+                  <button className="button" onClick={() => operatorElement('-')}>-</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'*'} onClick={inputElement}>x</button>
+                  <button className="button" onClick={() => operatorElement('*')}>x</button>
                 </div>
               </div>
 
               <div className="row m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'7'} onClick={inputElement}>7</button>
+                  <button className="button" onClick={() => inputElement('7')}>7</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'8'} onClick={inputElement}>8</button>
+                  <button className="button" onClick={() => inputElement('8')}>8</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'9'} onClick={inputElement}>9</button>
+                  <button className="button" onClick={() => inputElement('9')}>9</button>
                 </div>      
               </div>
 
               <div className="row m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'4'} onClick={inputElement}>4</button>
+                  <button className="button" onClick={() => inputElement('4')}>4</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'5'} onClick={inputElement}>5</button>
+                  <button className="button" onClick={() => inputElement('5')}>5</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'6'} onClick={inputElement}>6</button>
-                </div>
-              </div>
-
-              <div className="row m-0">
-                <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'1'} onClick={inputElement}>1</button>
-                </div>
-                <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'2'} onClick={inputElement}>2</button>
-                </div>
-                <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'3'} onClick={inputElement}>3</button>
+                  <button className="button" onClick={() => inputElement('6')}>6</button>
                 </div>
               </div>
 
               <div className="row m-0">
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'0'} onClick={inputElement}>0</button>
+                  <button className="button" onClick={() => inputElement('1')}>1</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button" value={'.'} onClick={inputElement}>.</button>
+                  <button className="button" onClick={() => inputElement('2')}>2</button>
                 </div>
                 <div className="col-2 my-2 mx-3 p-0">
-                  <button className="button ac-button" value={'ac'} onClick={clearInput}>AC</button>
+                  <button className="button" onClick={() => inputElement('3')}>3</button>
+                </div>
+              </div>
+
+              <div className="row m-0">
+                <div className="col-2 my-2 mx-3 p-0">
+                  <button className="button" onClick={() => inputElement('0')}>0</button>
+                </div>
+                <div className="col-2 my-2 mx-3 p-0">
+                  <button className="button" onClick={() => inputElement('.')}>.</button>
+                </div>
+                <div className="col-2 my-2 mx-3 p-0">
+                  <button className="button ac-button" onClick={(inputValue) => clearInput(inputValue)}>AC</button>
                 </div>
               </div>
             </div>
 
             <div className="col-2 p-0">
               <div className="my-2 mx-3 p-0">
-                  <button className="button" value={'/'} onClick={inputElement}>/</button>
+                  <button className="button" onClick={() => operatorElement('/')}>/</button>
               </div>
               <div className="my-3 my-2 mx-3 p-0">
-                <button className="button equal-button" onClick={resolveInput}>=</button>
+                <button className="button equal-button" onClick={() => resolveInput(inputValue)}>=</button>
               </div>
             </div>
 
