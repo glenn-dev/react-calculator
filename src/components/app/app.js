334a81818a1fb100d946c3d2e4d6d5168b8737fa
glenn-dev <glenn.marcano90@gmail.com>
2020-06-05 01:13:03 -0400

    Navbar added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import Calculator from './../calculator/calculator';
-import Footer from './../shared/footer/footer';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import React from "react";
+
+/* COMPONENTS */
+import Calculator from "./../calculator/calculator";
+import Footer from "./../shared/footer/footer";
+import Navbar from "./../shared/navbar/navbar";
+
+/* BOOTSTRAP */
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Container>
-        <Row className='mt-5'>
-          <div className='col-12'>
+        <Row className="mt-5">
+          <div className="col-12">
             <Calculator initialValue="0" />
           </div>
         </Row>
---
+++ src/components/shared/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid px-3">
+        <i class="fab fa-react fa-3x text-white"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
