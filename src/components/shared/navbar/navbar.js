87209e47d765a8255d4db774a966feaa319466ba
glenn-dev <glenn.marcano90@gmail.com>
2020-06-05 18:13:33 -0400

    App ready to deploy

--- src/components/shared/navbar/navbar.js
+++ src/components/shared/navbar/navbar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid px-3">
+    <div className="navbar navbar-dark bg-dark">
+      <div className="px-3">
         <i className="fab fa-react fa-3x text-white"></i>
       </div>
     </div>
